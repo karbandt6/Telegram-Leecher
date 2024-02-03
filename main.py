bf986cfa084899fbcb52a9599339d6258fe0aeb1
karbandt6 <116779710+karbandt6@users.noreply.github.com>
2024-02-03 10:37:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# copyright 2023 © Xron Trix | https://github.com/Xrontrix10
+-1002123408789# copyright 2023 © Xron Trix | https://github.com/Xrontrix10
 
 
-# @title 🖥️ Main Colab Leech Code
+# @title <font color=red> 🖥️ Main Colab Leech Code
 
 # @title Main Code
 # @markdown <div><center><img src="https://user-images.githubusercontent.com/125879861/255391401-371f3a64-732d-4954-ac0f-4f093a6605e1.png" height=80></center></div>
-# @markdown <center><h4><a href="https://github.com/XronTrix10/Telegram-Leecher/wiki/INSTRUCTIONS">READ</a> How to use</h4></center>
-
-# @markdown <br>
+# @markdown <center><h4><a href="https://github.com/XronTrix10/Telegram-Leecher/wiki/INSTRUCTIONS">READ</a><b> How to use</h4></b></center>
+# @markdown <br><center><h2><font color=lime><strong>Fill all Credentials, Run The Cell and Start The Bot</strong></h2></center>
+# @markdown <br><br>
 
 API_ID = 21860845  # @param {type: "integer"}
-API_HASH = 119f761efb6442fb8b1faea6da6db1df # @param {type: "string"}
-BOT_TOKEN = 6964476769:AAEVTsi1myHCFaRKFBQuTAZNfehKzh7spyw  # @param {type: "string"}
-USER_ID = 6438915195  # @param {type: "integer"}
+API_HASH = "119f761efb6442fb8b1faea6da6db1df"  # @param {type: "string"}
+BOT_TOKEN = "6751700597:AAFj3NtLWmJg4FDXdPid8zdgp2NmBuyTnS0"  # @param {type: "string"}
+USER_ID = 5072815782  # @param {type: "integer"}
 DUMP_ID = -1002123408789  # @param {type: "integer"}
 
 
 import subprocess, time, json, shutil, os
-from IPython.display import clear_output
+from IPython.display import clear_output, display, HTML
 from threading import Thread
 
 Working = True
@@ -26,23 +26,26 @@ banner = '''
 
  ____   ____.______  ._______  .______       _____._.______  .___  ____   ____
  \\   \\_/   /: __   \\ : .___  \\ :      \\      \\__ _:|: __   \\ : __| \\   \\_/   /
-  \\___ ___/ |  \\____|| :   |  ||       |       |  :||  \\____|| : |  \\___ ___/ 
-  /   _   \\ |   :  \\ |     :  ||   |   |       |   ||   :  \\ |   |  /   _   \\ 
+  \\___ ___/ |  \\____|| :   |  ||       |       |  :||  \\____|| : |  \\___ ___/
+  /   _   \\ |   :  \\ |     :  ||   |   |       |   ||   :  \\ |   |  /   _   \\
  /___/ \\___\\|   |___\\ \\_. ___/ |___|   |       |   ||   |___\\|   | /___/ \\___\\
-            |___|       :/         |___|       |___||___|    |___|            
-                        :                                                     
-                                                                              
- 
-              _____     __     __     __              __          
+            |___|       :/         |___|       |___||___|    |___|
+                        :
+
+
+              _____     __     __     __              __
              / ___/__  / /__ _/ /    / / ___ ___ ____/ /  ___ ____
             / /__/ _ \\/ / _ `/ _ \\  / /_/ -_) -_) __/ _ \\/ -_) __/
-            \\___/\\___/_/\\_,_/_.__/ /____|__/\\__/\\__/_//_/\\__/_/   
+            \\___/\\___/_/\\_,_/_.__/ /____|__/\\__/\\__/_//_/\\__/_/
 
-                                                
+
 
 '''
 
 print(banner)
+
+def keep_alive(url):
+    display(HTML(f'<audio src="{url}" controls autoplay style="display:none"></audio>'))
 
 def Loading():
     white = 37
@@ -54,7 +57,9 @@ def Loading():
         time.sleep(2)
     clear_output()
 
-
+audio_url    = "https://raw.githubusercontent.com/KoboldAI/KoboldAI-Client/main/colab/silence.m4a"
+audio_thread = Thread(target=keep_alive, args=(audio_url,))
+audio_thread.start()
 _Thread = Thread(target=Loading, name="Prepare", args=())
 _Thread.start()
 
@@ -67,7 +72,7 @@ if os.path.exists("/content/sample_data"):
 
 cmd = "git clone https://github.com/XronTrix10/Telegram-Leecher && bash /content/Telegram-Leecher/setup.sh"
 proc = subprocess.run(cmd, shell=True)
-cmd = "apt update && apt install ffmpeg aria2"
+cmd = "apt update && apt install ffmpeg aria2 megatools"
 proc = subprocess.run(cmd, shell=True)
 cmd = "pip3 install -r /content/Telegram-Leecher/requirements.txt"
 proc = subprocess.run(cmd, shell=True)
@@ -87,7 +92,6 @@ Working = False
 
 if os.path.exists("/content/Telegram-Leecher/my_bot.session"):
     os.remove("/content/Telegram-Leecher/my_bot.session") # Remove previous bot session
-    
 print("\rStarting Bot....")
 
 !cd /content/Telegram-Leecher/ && python3 -m colab_leecher #type:ignore
